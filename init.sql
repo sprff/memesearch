d99715f7e55b2150dd0811677e905148cb2975f4
Maxim Saprykin <saprykinoff@inbox.ru>
2025-03-20 20:53:08 +0300

    Init

---
+++ init.sql
@@ -0,0 +1,31 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS memes
+(
+    id VARCHAR(63) PRIMARY KEY,
+    board_id VARCHAR(63),
+    filename TEXT,
+    descriptions BYTEA
+);
+
+CREATE TABLE IF NOT EXISTS boards
+(
+    id VARCHAR(63) PRIMARY KEY,
+    owner_id VARCHAR(63),
+    name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id VARCHAR(63) PRIMARY KEY,
+    login TEXT,
+    password TEXT
+);
+
+CREATE TABLE IF NOT EXISTS medias
+(
+    id VARCHAR(63) PRIMARY KEY,
+    body BYTEA
+)
+
+COMMIT;
