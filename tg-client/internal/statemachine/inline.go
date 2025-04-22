020692577c00030ca38665292f04d47a86902e14
Maxim Saprykin <saprykinoff@inbox.ru>
2025-04-22 16:40:13 +0300

    add photo/video flags

--- tg-client/internal/statemachine/inline.go
+++ tg-client/internal/statemachine/inline.go
@@ -2,35 +2,66 @@ package statemachine
 
 import (
 	"api-client/pkg/models"
+	"errors"
 	"fmt"
 	"log/slog"
+	"strconv"
+	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func processInline(q *tgbotapi.InlineQuery, r RequestContext) {
 	ctx := r.Ctx
-	memes, err := r.ApiClient.SearchMemes(ctx, 1, 10, q.Query)
+
+	page := 1
+	if q.Offset != "" {
+		var err error
+		page, err = strconv.Atoi(q.Offset)
+		if err != nil {
+			// очень жаль
+		}
+	}
+	spl := strings.Split(q.Query, "!")
+	req := spl[0]
+	flags := "pv"
+	if len(spl) > 1 {
+		req = spl[1]
+		flags = spl[0]
+	}
+
+	memes, err := r.ApiClient.SearchMemes(ctx, page, 10, req)
 	if err != nil {
 		slog.ErrorContext(ctx, "Can't search", "err", err)
 		return
 	}
-
+	newOffset := ""
+	if len(memes) != 0 {
+		newOffset = fmt.Sprintf("%d", page+1)
+	}
 	inlineResponse := []any{}
 	for _, meme := range memes {
-		entry, err := prepareMeme(meme.Meme, r)
+		entry, err := prepareMeme(meme.Meme, r, flags)
 
 		if err != nil {
-			slog.ErrorContext(ctx, "Can't prepare meme", "err", err)
+			if err != ErrSkipped {
+				slog.ErrorContext(ctx, "Can't prepare meme", "err", err)
+			}
 			continue
 		}
 		inlineResponse = append(inlineResponse, entry)
 	}
-	r.Bot.AnswerInlineQuery(ctx, q.ID, inlineResponse, "")
+
+	r.Bot.AnswerInlineQuery(ctx, q.ID, inlineResponse, newOffset)
 }
 
-func prepareMeme(meme models.Meme, r RequestContext) (any, error) {
+var ErrSkipped = errors.New("skip")
+
+func prepareMeme(meme models.Meme, r RequestContext, flags string) (any, error) {
 	ctx := r.Ctx
+	photos := strings.Contains(flags, "p")
+	videos := strings.Contains(flags, "v")
+
 	cm, err := r.Bot.GetCachedMedia(string(meme.ID), func() ([]byte, error) {
 		media, err := r.ApiClient.GetMediaByID(ctx, models.MediaID(meme.ID))
 		return media.Body, err
@@ -40,9 +71,15 @@ func prepareMeme(meme models.Meme, r RequestContext) (any, error) {
 	}
 	switch cm.Type {
 	case "photo":
+		if !photos {
+			return nil, ErrSkipped
+		}
 		photo := tgbotapi.NewInlineQueryResultCachedPhoto(string(meme.ID), cm.ID)
 		return photo, nil
 	case "video":
+		if !videos {
+			return nil, ErrSkipped
+		}
 		video := tgbotapi.NewInlineQueryResultCachedVideo(string(meme.ID), cm.ID, "Title")
 		return video, nil
 	default:
